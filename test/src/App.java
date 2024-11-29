97796dde838744d9aa2a0887a8d51a94d137b6d1
Lunar <84755426+TrinityRei@users.noreply.github.com>
2024-11-29 14:47:27 +0000

    FNAKDALDS

--- test/src/App.java
+++ test/src/App.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
+
+    public static String[] servers = { "cloud-1", "cloud-2", "cloud-3", "vip-1", "vip-2" };
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -19,33 +22,42 @@ public class App {
     public static void getCredentials(Scanner input) {
         System.out.println("Enter your name! [ANY]");
         String username = input.next();
-        boolean serverChecker = false;
-        String server = "";
-        
-        while(!serverChecker == true) {
-            System.out.println("Enter your server!");
-            server = input.next();
+        String reqServer = "";
 
-            if(!server.equals("cloud-1")){
+        reqServer = checkServer(input, reqServer);
+        login(username, reqServer);
+    }
+
+    public static String checkServer(Scanner input, String reqServer) {
+        boolean isServerValid = false;
+
+        while (!isServerValid == true) {
+            System.out.println("Enter your server!");
+            reqServer = input.next();
+
+            Arrays.parallelSort(servers);
+            int index = Arrays.binarySearch(servers, reqServer);
+
+            if (index < 0) {
                 System.out.println("\nInvalid server!");
             } else {
-                serverChecker = true;
+                isServerValid = true;
             }
         }
 
-        login(username, server);
+        return reqServer;
     }
 
     public static void startup(Scanner input) {
         String status = "";
 
-        while(!status.toLowerCase().equals("yes") || status.toLowerCase().equals("no")){
+        while (!status.toLowerCase().equals("yes") || status.toLowerCase().equals("no")) {
             System.out.println("Would you like to go online? [YES/NO]");
             status = input.nextLine();
-            if(status.toLowerCase().equals("no")){
+            if (status.toLowerCase().equals("no")) {
                 System.exit(0);
             }
-            if(!status.toLowerCase().equals("yes")) {
+            if (!status.toLowerCase().equals("yes")) {
                 System.out.println("\nInvalid option!");
             }
         }
